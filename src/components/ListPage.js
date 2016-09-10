fe503b5a2065dd4b08fdc47214595f0f92659bc2
Rene Brandel <renebrandel@outlook.com>
2016-09-10 17:13:31 +0200

    Adding missing file

---
+++ src/components/ListPage.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Link } from 'react-router'
+import Post from '../components/Post'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
+
+class ListPage extends React.Component {
+
+  static propTypes = {
+    data: React.PropTypes.object,
+  }
+
+  render () {
+    if (this.props.data.loading) {
+      return (<div>Loading</div>)
+    }
+    return (
+      <div className='w-100 flex justify-center'>
+        <Link to='/create' className='fixed bg-white top-0 right-0 pa4 ttu dim black no-underline'>
+          + New Post
+        </Link>
+        <div className='w-100' style={{ maxWidth: 400 }}>
+          {this.props.data.allPosts.reverse().map((post) =>
+            <Post key={post.id} post={post} refresh={() => this.props.data.refetch()} />
+          )}
+        </div>
+      </div>
+    )
+  }
+}
+
+const FeedQuery = gql`query { allPosts { id imageUrl description } }`
+
+const ListPageWithData = graphql(FeedQuery)(ListPage)
+
+export default ListPageWithData
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ListPage from './components/ListPage'
+import CreatePage from './components/CreatePage'
+import { Router, Route, browserHistory } from 'react-router'
+import ApolloClient, { createNetworkInterface } from 'apollo-client'
+import { ApolloProvider } from 'react-apollo'
+import './index.css'
+
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface('https://api.graph.cool/simple/v1/cis7q7f5r27ig01420766c54k'),
+})
+
+ReactDOM.render((
+  <ApolloProvider client={client}>
+    <Router history={browserHistory}>
+      <Route path='/' component={ListPage} />
+      <Route path='/create' component={CreatePage} />
+    </Router>
+  </ApolloProvider>
+  ),
+  document.getElementById('root')
+)
