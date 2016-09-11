7d3809afa613db205a71a563a0b6e72181ead2b7
Rene Brandel <renebrandel@outlook.com>
2016-09-11 12:11:50 +0200

    Added x-graphcool-source

--- src/index.js
+++ src/index.js
@@ -8,7 +8,19 @@ import { ApolloProvider } from 'react-apollo'
 import 'tachyons'
 import './index.css'
 
-const networkInterface = createNetworkInterface('https://api.graph.cool/simple/v1/__PROJECT_ID__')
+const networkInterface = createNetworkInterface('https://api.graph.cool/simple/v1/cis7q7f5r27ig01420766c54k')
+
+// This is to let the server know that the example app has started. (Not necessary for normal projects)
+networkInterface.use([{
+  applyMiddleware (req, next) {
+    if (!req.options.headers) {
+      // Create the header object if needed.
+      req.options.headers = {}
+    }
+    req.options.headers['x-graphcool-source'] = 'example:react-apollo-apollo'
+    next()
+  },
+}])
 
 const client = new ApolloClient({ networkInterface })
 
