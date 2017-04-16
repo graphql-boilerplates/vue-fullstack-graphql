491e5a47e3ed0a4fd1c781eefb2c332dae68c741
Nikolas Burk <nikolas.burk@gmail.com>
2017-04-16 11:34:31 +0200

    added placeholder for simple api endpoint

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ import { ApolloProvider } from 'react-apollo'
 import 'tachyons'
 import './index.css'
 
-const networkInterface = createNetworkInterface({ uri: process.env.REACT_APP_GRAPHQL_ENDPOINT })
+// Paste your endpoint for the Simple API here.
+// Info: https://github.com/graphcool-examples/react-apollo-instagram-example#2-create-graphql-api-with-graphcool
+const networkInterface = createNetworkInterface({ uri: '__SIMPLE_API_ENDPOINT__' });
 
 const client = new ApolloClient({ networkInterface })
 
