798a1353cdc9d45171b61399648fa689f84a013a
timsuchanek <tim.suchanek@gmail.com>
2018-04-24 18:33:39 +0200

    fix basic example

--- advanced/.install/index.js
+++ advanced/.install/index.js
@@ -31,7 +31,7 @@ Next steps:
   1. Change directory: \`cd ${projectDir}/server\`
   2. Start local server: \`yarn start\` (you can now open a Playground at http://localhost:4000)
   3. Change directory: \`cd ..\`
-  4. Start React app: \`yarn start\`
+  4. Start VueJS app: \`yarn start\`
   5. Open browser: http://localhost:3000
 `)
 }
--- basic/.install/index.js
+++ basic/.install/index.js
@@ -4,34 +4,42 @@ const {
   deploy,
   writeEnv,
   getInfo,
+  makeSandboxEndpoint,
 } = require('graphql-boilerplate-install')
 
 module.exports = async ({ project, projectDir }) => {
   const templateName = 'graphql-boilerplate'
 
+  const endpoint = await makeSandboxEndpoint(project)
+
+  process.chdir('server/')
   replaceInFiles(
-    ['server/src/index.js', 'server/package.json', 'server/database/prisma.yml'],
+    ['src/index.js', 'package.json', 'database/prisma.yml'],
     templateName,
     project,
   )
+  replaceInFiles(['src/index.js'], '__PRISMA_ENDPOINT__', endpoint)
+  replaceInFiles(['database/prisma.yml'], '__PRISMA_ENDPOINT__', endpoint)
 
   console.log('Running $ prisma deploy...')
-
-  process.chdir('server/')
 
   await deploy(false)
   const info = await getInfo()
 
   process.chdir('../')
 
-  replaceInFiles(['server/src/index.js'], '__PRISMA_ENDPOINT__', info.httpEndpoint)
+  replaceInFiles(
+    ['server/src/index.js'],
+    '__PRISMA_ENDPOINT__',
+    info.httpEndpoint,
+  )
 
   console.log(`\
 Next steps:
   1. Change directory: \`cd ${projectDir}/server\`
   2. Start local server: \`yarn start\` (you can now open a Playground at http://localhost:4000)
   3. Change directory: \`cd ..\`
-  4. Start VueJs app: \`yarn start\`
-  5. Open browser: http://localhost:8080
+  4. Start VueJS app: \`yarn start\`
+  5. Open browser: http://localhost:3000
 `)
 }
